641279bab669584a64682b5e44c499a605e6f40d
mosqueda.christopher <61306146+cmosqueda1@users.noreply.github.com>
2025-12-16 09:15:18 -0600

    Delete api/job-result.js

--- api/job-result.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  const job = global.jobs?.[req.query.jobId];
-  if (!job || job.status !== "done") {
-    return res.status(400).json({ error: "Not ready" });
-  }
-  res.json(job.results);
-}
